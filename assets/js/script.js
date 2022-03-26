783735b445801a1f1d97c0ca278e79389a0d4bb6
randronaco <randronaco1027@gmail.com>
2022-03-26 15:31:09 -0400

    added local storage properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,64 @@
-$("#saveBtn").click(function(){
+$("#saveBtn").click(function () {
     //local storage
 })
 
-$("#currentDay").text(moment().format("MMM Do YYYY"))
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-console.log(moment().set("hour", 9))
-console.log(moment().tz("America/Los_Angeles").format());
+console.log(moment().set('month', 3))
+
+$(textarea1).addClass(".present");
+
+$("#clearTasks").click(function () {
+    localStorage.clear()
+})
+
+$("#saveBtn1").click(function () {
+    var content1 = $.trim($("#textarea1").val())
+    localStorage.setItem ("content1", JSON.stringify(content1));
+});
+$("#saveBtn2").click(function () {
+    var content2 = $.trim($("#textarea2").val())
+    localStorage.setItem ("content2", JSON.stringify(content2));
+});
+$("#saveBtn3").click(function () {
+    var content3 = $.trim($("#textarea3").val())
+    localStorage.setItem ("content3", JSON.stringify(content3));
+});
+$("#saveBtn4").click(function () {
+    var content4 = $.trim($("#textarea4").val())
+    localStorage.setItem ("content4", JSON.stringify(content4));
+});
+$("#saveBtn5").click(function () {
+    var content5 = $.trim($("#textarea5").val())
+    localStorage.setItem ("content5", JSON.stringify(content5));
+});
+$("#saveBtn6").click(function () {
+    var content6 = $.trim($("#textarea6").val())
+    localStorage.setItem ("content6", JSON.stringify(content6));
+});
+$("#saveBtn7").click(function () {
+    var content7 = $.trim($("#textarea7").val())
+    localStorage.setItem ("content7", JSON.stringify(content7));
+});
+$("#saveBtn8").click(function () {
+    var content8 = $.trim($("#textarea8").val())
+    localStorage.setItem ("content8", JSON.stringify(content8));
+});
+$("#saveBtn9").click(function () {
+    var content9 = $.trim($("#textarea9").val())
+    localStorage.setItem ("content9", JSON.stringify(content9));
+});
 
 
-$("#9am").text(moment().set("hour", 9))
-$("#10am").text(moment().format("MMM Do YYYY"))
-$("#11am").text(moment().format("MMM Do YYYY"))
-$("#12pm").text(moment().format("MMM Do YYYY"))
-$("#1pm").text(moment().format("MMM Do YYYY"))
-$("#2pm").text(moment().format("MMM Do YYYY"))
-$("#3pm").text(moment().format("MMM Do YYYY"))
-$("#4pm").text(moment().format("MMM Do YYYY"))
-$("#5pm").text(moment().format("MMM Do YYYY"))
+$("#textarea1").val(localStorage.getItem ("content1"));
+$("#textarea2").val(localStorage.getItem ("content2"));
+$("#textarea3").val(localStorage.getItem ("content3"));
+$("#textarea4").val(localStorage.getItem ("content4"));
+$("#textarea5").val(localStorage.getItem ("content5"));
+$("#textarea6").val(localStorage.getItem ("content6"));
+$("#textarea7").val(localStorage.getItem ("content7"));
+$("#textarea8").val(localStorage.getItem ("content8"));
+$("#textarea9").val(localStorage.getItem ("content9"));
 
-// // Add auditing WITH TIMER to boxes
-// var auditTask = function(taskEl) {
-
-//     // get date from task element
-//     var date = $(taskEl)
-//       .find("span")
-//       .text()
-//       .trim();
-  
-//     console.log(date);
-  
-//     // convert to moment object at 5:00pm
-//     var time = moment(date, "L").set("hour", 17);
-  
-//     console.log(time);
-  
-//     // remove any old classes from element
-//     $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-  
-//     // apply new class if task is near/over due date
-//     if (moment().isAfter(time)) {
-//       $(taskEl).addClass("list-group-item-danger");
-//     } 
-//     else if (Math.abs(moment().diff(time, "days")) <= 2) {
-//       $(taskEl).addClass("list-group-item-warning");
-//     }
-//   };
-
-// // Enable boxes to be edited
-// $(".list-group").on("blur", "textarea", function() {
-//     // get current value of textarea
-//     var text = $(this).val();
-  
-//     // get status type and position in the list
-//     var status = $(this)
-//       .closest(".list-group")
-//       .attr("id")
-//       .replace("list-", "");
-//     var index = $(this)
-//       .closest(".list-group-item")
-//       .index();
-  
-//     // update task in array and re-save to localstorage
-//     tasks[status][index].text = text;
-//     saveTasks();
-  
-//     // recreate p element
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(text);
-  
-//     // replace textarea with new content
-//     $(this).replaceWith(taskP);
-//   });
-
-// // Save buttons
-// var saveTasks = function() {
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//   };
+//   $("#textarea1").val(localStorage.getItem ("abc")); //Get
+//   localStorage.setItem ($("#textarea1").val());
