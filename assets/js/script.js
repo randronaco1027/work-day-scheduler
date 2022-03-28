62ad133f3cebf4a3d609191d8e0cac030fcf88a8
randronaco <randronaco1027@gmail.com>
2022-03-28 01:23:34 -0400

    added time functions to change styles of textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
-$("#saveBtn").click(function () {
-    //local storage
-})
-
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
-console.log(moment().set('month', 3))
-
-$(textarea1).addClass(".present");
 
 $("#clearTasks").click(function () {
     localStorage.clear()
@@ -60,5 +52,100 @@ $("#textarea7").val(localStorage.getItem ("content7"));
 $("#textarea8").val(localStorage.getItem ("content8"));
 $("#textarea9").val(localStorage.getItem ("content9"));
 
-//   $("#textarea1").val(localStorage.getItem ("abc")); //Get
-//   localStorage.setItem ($("#textarea1").val());
+var currentTime = moment().format("LT");
+
+var timeslot1 = new Date();
+timeslot1.setHours(9,0,0)
+$("#9am").text(moment(timeslot1).format("LT"))
+if (currentTime <= moment(timeslot1).format("LT")) {
+    $("#textarea1").removeClass("future");
+    $("#textarea1").addClass("past");
+    console.log("Past")
+    console.log(timeslot1)
+}
+
+var timeslot2 = new Date();
+timeslot2.setHours(10,0,0)
+$("#10am").text(moment(timeslot2).format("LT"))
+if (currentTime <= moment(timeslot2).format("LT")) {
+    $("#textarea2").removeClass("future");
+    $("#textarea2").addClass("past");
+    console.log("Past")
+}
+else {
+    console.log("False")
+    console.log(timeslot2)
+}
+
+var timeslot3 = new Date();
+timeslot3.setHours(11,0,0)
+$("#11am").text(moment(timeslot3).format("LT"))
+if (currentTime <= moment(timeslot3).format("LT")) {
+    $("#textarea3").removeClass("future");
+    $("#textarea3").addClass("past");
+    console.log("Past")
+}
+else {
+    console.log("False")
+    console.log(timeslot3)
+}
+
+var timeslot4 = new Date();
+timeslot4.setHours(12,0,0)
+$("#12pm").text(moment(timeslot4).format("LT"))
+if (currentTime <= moment(timeslot4).format("LT")) {
+    $("#textarea4").removeClass("future");
+    $("#textarea4").addClass("past");
+    console.log("Past")
+    console.log(timeslot4)
+}
+
+var timeslot5 = new Date();
+timeslot5.setHours(13,0,0)
+$("#1pm").text(moment(timeslot5).format("LT"))
+if (currentTime <= moment(timeslot5).format("LT")) {
+    $("#textarea5").removeClass("future");
+    $("#textarea5").addClass("past");
+    console.log("Past")
+    console.log(timeslot5)
+}
+
+var timeslot6 = new Date();
+timeslot6.setHours(14,0,0)
+$("#2pm").text(moment(timeslot6).format("LT"))
+if (currentTime <= moment(timeslot6).format("LT")) {
+    $("#textarea6").removeClass("future");
+    $("#textarea6").addClass("past");
+    console.log("Past")
+    console.log(timeslot6)
+}
+
+var timeslot7 = new Date();
+timeslot7.setHours(15,0,0)
+$("#3pm").text(moment(timeslot7).format("LT"))
+if (currentTime <= moment(timeslot7).format("LT")) {
+    $("#textarea7").removeClass("future");
+    $("#textarea7").addClass("past");
+    console.log("Past")
+    console.log(timeslot7)
+}
+
+var timeslot8 = new Date();
+timeslot8.setHours(16,0,0)
+$("#4pm").text(moment(timeslot8).format("LT"))
+if (currentTime <= moment(timeslot8).format("LT")) {
+    $("#textarea8").removeClass("future");
+    $("#textarea8").addClass("past");
+    console.log("Past")
+    console.log(timeslot8)
+}
+
+var timeslot9 = new Date();
+timeslot9.setHours(17,0,0)
+$("#5pm").text(moment(timeslot9).format("LT"))
+if (currentTime <= moment(timeslot9).format("LT")) {
+    $("#textarea9").removeClass("future");
+    $("#textarea9").addClass("past");
+    console.log("Past")
+    console.log(timeslot9)
+}
